d675d5977bcf1cb9b2c07bc4cc9af175cd1f436d
Maugemus <maximus.paajaste@hotmail.com>
2024-10-13 22:13:49 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Valikon avaaminen
+function openMenu() {
+    document.getElementById("overlay-menu").style.width = "100%";
+}
+
+// Valikon sulkeminen
+function closeMenu() {
+    document.getElementById("overlay-menu").style.width = "0%";
+}
