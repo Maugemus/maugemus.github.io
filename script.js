5db074156e30e0f99637ec12b0e74b7f569872a2
Maugemus <maximus.paajaste@hotmail.com>
2024-10-13 22:51:12 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-// Valikon avaaminen
 function openMenu() {
     document.getElementById("overlay-menu").style.width = "100%";
 }
 
-// Valikon sulkeminen
 function closeMenu() {
     document.getElementById("overlay-menu").style.width = "0%";
 }
